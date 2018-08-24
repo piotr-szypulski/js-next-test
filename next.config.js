2b642eda6f6ead433cf065b2e55204f4edd00732
Maciej Plusa <maciej.plusa@intive.com>
2018-08-24 13:34:53 +0200

    Add support for static files export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const withCSS = require('@zeit/next-css');
+const fetch = require('isomorphic-unfetch')
 
 module.exports = withCSS({
   cssModules: false,
@@ -15,4 +16,27 @@ module.exports = withCSS({
     });
     return config;
   },
+  async exportPathMap() {
+    const url = `https://public-api.wordpress.com/wp/v2/sites/mygatsby.home.blog/posts?_embed`;
+    // we fetch our list of posts, this allow us to dynamically generate the exported pages
+    const response = await fetch(url)
+    const postList = await response.json()
+
+    // tranform the list of posts into a map of pages with the pathname `/post/:id`
+    const pages = postList.reduce(
+      (pages, post) =>
+        Object.assign({}, pages, {
+          [`/blog/${post.slug}`]: {
+            page: '/blog-post',
+            query: { slug: post.slug }
+          },
+        }),
+      {},
+    );
+
+    // combine the map of post pages with the home
+    return Object.assign({}, pages, {
+      '/': { page: '/' }
+    })
+  }
 });
